93e88557a16d1b18db55195b6e81555aaf539fee
Ramon <r.amonhagenaars@gmail.com>
2019-03-04 16:50:09 +0100

    Package fix.

--- setup.py
+++ setup.py
@@ -4,14 +4,17 @@ with open('README.rst', 'r') as fh:
     long_description = fh.read()
 
 setup(name='jsons',
-      version='0.7.1',
+      version='0.7.2',
       author='Ramon Hagenaars',
       author_email='ramon.hagenaars@gmail.com',
       description='For serializing Python objects to JSON (dicts) and back',
       long_description=long_description,
       long_description_content_type='text/x-rst',
       url='https://github.com/ramonhagenaars/jsons',
-      packages=['jsons'],
+      packages=['jsons',
+                'jsons.classes',
+                'jsons.deserializers',
+                'jsons.serializers'],
       zip_safe=False,
       classifiers=(
           'Programming Language :: Python :: 3',
