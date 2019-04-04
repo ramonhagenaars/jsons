f9b91a05d110ed50d899d51e74449db28a2913c7
Ramon <r.amonhagenaars@gmail.com>
2019-04-04 20:46:03 +0200

    Testing version specific stuff.

--- tests/test_specific_versions.py
+++ tests/test_specific_versions.py
@@ -17,11 +17,37 @@ path = Path(__file__).parent.parent.joinpath(Path('test_resources')).absolute()
 sys.path.insert(0, str(path))
 
 
+def only_version_3(minor_version: int, and_above: bool = False):
+    def _decorator(decorated):
+        dont_skip = (sys.version_info.minor >= minor_version if and_above
+                     else minor_version == sys.version_info.minor)
+        reason = 'Only 3.{}{}'.format(minor_version, '+' if and_above else '')
+
+        @skipUnless(dont_skip, reason=reason)
+        def _wrapper(*args, **kwargs):
+            return decorated(*args, **kwargs)
+
+        return _wrapper
+    return _decorator
+
+
 class TestSpecificVersions(TestCase):
 
-    @skipUnless(dataclass, reason='Only 3.7+')
+    @only_version_3(7)
+    def test_simple_dump_and_load_dataclass_with_future_import(self):
+        import version_37
+        from version_with_dataclasses import Person
+        p = Person('John')
+        dumped = jsons.dump(p)
+        loaded = jsons.load(dumped, Person)
+        self.assertEqual(p.name, loaded.name)
+
+        with self.assertRaises(SignatureMismatchError):
+            jsons.load({'name': 'John', 'age': 88}, Person, strict=True)
+
+    @only_version_3(6, and_above=True)
     def test_simple_dump_and_load_dataclass(self):
-        from python37 import Person
+        from version_with_dataclasses import Person
         p = Person('John')
         dumped = jsons.dump(p)
         loaded = jsons.load(dumped, Person)
