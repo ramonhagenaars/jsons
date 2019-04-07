babf6cfac9d188f3a6508ad8c1a7fc3c73a5a9fd
Ramon <r.amonhagenaars@gmail.com>
2019-04-07 10:28:06 +0200

    Added tests.

--- tests/test_verbosity.py
+++ tests/test_verbosity.py
@@ -7,5 +7,6 @@ class TestVerbosity(TestCase):
         self.assertEqual(Verbosity.WITH_DUMP_TIME, Verbosity.from_value(Verbosity.WITH_DUMP_TIME))
         self.assertEqual(Verbosity.WITH_NOTHING, Verbosity.from_value(False))
         self.assertEqual(Verbosity.WITH_NOTHING, Verbosity.from_value(None))
+        self.assertEqual(Verbosity.WITH_NOTHING, Verbosity.from_value([]))
         self.assertEqual(Verbosity.WITH_EVERYTHING, Verbosity.from_value(True))
         self.assertEqual(Verbosity.WITH_EVERYTHING, Verbosity.from_value([1, 2, 3]))
