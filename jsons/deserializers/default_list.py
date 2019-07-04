a97441220d1be86d1a791a957212d166fd73512a
Ramon <p8u7wAPC5Pg9HYkkCkzA>
2019-07-04 20:29:56 +0200

    Support for multithreading in the list deserializer.

--- jsons/deserializers/default_list.py
+++ jsons/deserializers/default_list.py
@@ -1,11 +1,18 @@
+from threading import Thread
 from jsons._load_impl import load
 
 
-def default_list_deserializer(obj: list, cls: type = None, **kwargs) -> list:
+def default_list_deserializer(
+        obj: list,
+        cls: type = None,
+        *,
+        threads: int = 1,
+        **kwargs) -> list:
     """
     Deserialize a list by deserializing all items of that list.
     :param obj: the list that needs deserializing.
     :param cls: the type optionally with a generic (e.g. List[str]).
+    :param threads: the number of threads that is allowed to use.
     :param kwargs: any keyword arguments.
     :return: a deserialized list instance.
     """
@@ -16,4 +23,55 @@ def default_list_deserializer(obj: list, cls: type = None, **kwargs) -> list:
         # Mark the cls as 'inferred' so that later it is known where cls came
         # from and the precedence of classes can be determined.
         kwargs_['_inferred_cls'] = True
-    return [load(x, cls_, **kwargs_) for x in obj]
+
+    func = _single_threaded if threads == 1 else _multi_threaded
+    return func(obj, cls_, threads, **kwargs_)
+
+
+def _single_threaded(
+        # Load the elements of the list in a single thread.
+        obj: list,
+        cls: type,
+        threads: int,
+        **kwargs):
+    return [load(x, cls, threads=1, **kwargs) for x in obj]
+
+
+def _multi_threaded(
+        obj: list,
+        cls: type,
+        threads: int,
+        **kwargs):
+    # Load the elements of the list with multiple threads.
+
+    # First, create a list with the correct size for the threads to fill.
+    result = [0] * len(obj)
+
+    # Divide the list in parts.
+    slice_size = int(len(obj) / threads)
+    rest_size = len(obj) % threads
+
+    # Start the threads and store them to join them later.
+    threads_instances = []
+    for i in range(threads):
+        start = i
+        end = (i + 1) * slice_size
+        if i == 0:
+            end += rest_size
+        thread = Thread(
+            target=_fill,
+            args=(obj, cls, result, start, end, kwargs))
+        thread.start()
+        threads_instances.append(thread)
+
+    for thread in threads_instances:
+        thread.join()
+
+    return result
+
+
+def _fill(obj: list, cls: type, result: list, start: int, end: int, kwargs: dict):
+    # Fill result with the loaded objects of obj within the range start - end.
+    for i_ in range(start, end):
+        loaded = load(obj[i_], cls, **kwargs)
+        result[i_] = loaded
