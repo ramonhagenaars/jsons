ed5b6e24db20e568982ff068ffa1ddbf7901ded1
Ramon <p8u7wAPC5Pg9HYkkCkzA>
2019-07-14 13:58:09 +0200

    Added a test.

--- tests/test_various.py
+++ tests/test_various.py
@@ -93,3 +93,10 @@ class TestVarious(TestCase):
 
         loaded = jsons.load({'a': None}, C)
         self.assertEqual(None, loaded.a)
+
+    def test_deserialize_none(self):
+        fork_inst = jsons.fork()
+        jsons.set_deserializer(lambda *_, **__: 42,
+                               cls=None, fork_inst=fork_inst)
+        self.assertEqual(42, jsons.load('Anything', cls=NoneType,
+                                        fork_inst=fork_inst))
