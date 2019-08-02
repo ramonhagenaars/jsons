f92d98af069bf573b2faebd45bd5091f1e3e5559
Robin Klaassen <13085028+robinklaassen@users.noreply.github.com>
2019-08-02 15:32:38 +0200

    Use assertDictEqual for better effect

--- tests/test_complex_number.py
+++ tests/test_complex_number.py
@@ -6,14 +6,14 @@ class TestComplexNumber(TestCase):
     def test_dump_complex_number(self):
         a: complex = 5 + 3j
         dumped = jsons.dump(a)
-        self.assertEqual(dumped, {'imag': 3.0, 'real': 5.0})
+        self.assertDictEqual(dumped, {'real': 5.0, 'imag': 3.0})
 
     def test_dump_complex_number_property(self):
         class A:
             b: complex = 2 + 4j
 
         dumped = jsons.dump(A())
-        self.assertEqual(dumped, {'b': {'imag': 4.0, 'real': 2.0}})
+        self.assertDictEqual(dumped, {'b': {'real': 2.0, 'imag': 4.0}})
 
     def test_load_complex_number(self):
         dumped = {'imag': 2.0, 'real': 1.0}
