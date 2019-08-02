b8e5586dee9aaa6ccf8bbc7007acc67ed362a205
Robin Klaassen <13085028+robinklaassen@users.noreply.github.com>
2019-08-02 16:41:17 +0200

    More explicit checks

--- jsons/deserializers/default_complex.py
+++ jsons/deserializers/default_complex.py
@@ -1,10 +1,25 @@
 from typing import Dict
 
 
-def default_complex_deserializer(obj: Dict[str, float], cls: type = complex, **kwargs) -> complex:
-    print("Complex deserializer was called")
-    print(f"Object is {type(obj)}: {obj}")
-    print(f"Class is {type(cls)}: {cls}")
+def default_complex_deserializer(obj: Dict[str, float],
+                                 cls: type = complex,
+                                 **kwargs) -> complex:
+    """
+    Deserialize a dictionary with 'real' and 'imag' keys to a complex number.
+    :param obj: the dict that is to be deserialized.
+    :param cls: not used.
+    :param kwargs: not used.
+    :return: an instance of ``complex``.
+    """
     real = obj.get('real')
     imag = obj.get('imag')
+    clean_obj = {'real': real, 'imag': imag}
+    for key, value in clean_obj.items():
+        if not value:
+            raise AttributeError(f"Cannot deserialize {obj} to a complex number, does not contain key '{key}'")
+        try:
+            float(value)
+        except TypeError:
+            raise AttributeError(f"Cannot deserialize {obj} to a complex number, can't cast value of '{key}' to float")
+
     return complex(real, imag)
--- tests/test_complex_number.py
+++ tests/test_complex_number.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 import jsons
+from jsons import DeserializationError
 
 
 class TestComplexNumber(TestCase):
@@ -16,6 +17,14 @@ class TestComplexNumber(TestCase):
         self.assertDictEqual(dumped, {'b': {'real': 2.0, 'imag': 4.0}})
 
     def test_load_complex_number(self):
-        dumped = {'imag': 2.0, 'real': 1.0}
+        dumped = {'real': 1.0, 'imag': 2.0}
         loaded = jsons.load(dumped, complex)
         self.assertEqual(loaded, 1+2j)
+
+        bad_keys_dump = {'some_key': 3.0}
+        with self.assertRaises(DeserializationError):
+            jsons.load(bad_keys_dump, complex)
+
+        bad_types_dump = {'real': 'some_string', 'imag': {'a': 2}}
+        with self.assertRaises(DeserializationError):
+            jsons.load(bad_types_dump, complex)
