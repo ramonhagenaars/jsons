7788a82b84b76e515ad7b5801c46f4a869c40acf
George Harker <george@george-graphics.co.uk>
2020-10-17 08:07:01 +0100

    add test for coverage

---
+++ tests/test_dataclass_postponed.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import List, Dict, Optional
+from unittest import TestCase
+
+import jsons
+
+class TestObject(TestCase):
+    def test_dump_object(self):
+        @dataclass
+        class A:
+            a: Optional[int] = 42
+
+        obj = A()
+        exp = {'a': 42}
+        dump = jsons.dump(obj)
+        self.assertDictEqual(exp, dump)
+
