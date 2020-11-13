b85d41c6ad73a6ce64f98a1dcda00748d29cd635
George Harker <george@george-graphics.co.uk>
2020-11-13 04:49:23 +0000

    fix test to actually recreate issue

---
+++ test_resources/postposned_dataclass.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import List, Dict, Optional
+
+@dataclass
+class A:
+    a: Optional[int] = 42
+
--- tests/test_dataclass_postponed.py
+++ tests/test_dataclass_postponed.py
@@ -8,14 +8,22 @@ from jsons._compatibility_impl import Flag
 
 import jsons
 
+from test_resources.postposned_dataclass import A
+
 class TestObject(TestCase):
     def test_dump_object(self):
         @dataclass
-        class A:
-            a: Optional[int] = 42
+        class Wrap:
+            a: A
 
-        obj = A()
-        exp = {'a': 42}
+            def __init__(self, a : Optional[A] = None):
+                self.a = a if a is not None else A()
+
+        obj = Wrap()
+        exp = {'a' : {'a': 42} }
         dump = jsons.dump(obj)
         self.assertDictEqual(exp, dump)
+        
+        undump = jsons.load(dump, cls = Wrap)
+        self.assertEqual(undump, obj)
 
