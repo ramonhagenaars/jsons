1ff7e427271c1f9f01579d955ab821aa7a5d53eb
Ramon <p8u7wAPC5Pg9HYkkCkzA>
2021-10-23 16:42:12 +0200

    Made the python version classifiers come from the _package_info

--- setup.py
+++ setup.py
@@ -47,11 +47,7 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
+        *['Programming Language :: Python :: {}'.format(version)
+          for version in meta_info['__python_versions__']],
     ]
 )
