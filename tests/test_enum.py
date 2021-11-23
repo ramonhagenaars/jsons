2f1a022a75d564184bad5a0cab52725064dde113
George Harker <george@george-graphics.co.uk>
2021-11-22 16:15:40 -0800

    int enums should be serialized as

--- tests/test_enum.py
+++ tests/test_enum.py
@@ -1,4 +1,4 @@
-from enum import Enum
+from enum import Enum, IntEnum
 from unittest import TestCase
 
 import jsons
@@ -27,3 +27,26 @@ class TestEnum(TestCase):
             self.assertEqual(E.y, jsons.load(2, E, use_enum_name=True))
         with self.assertRaises(DeserializationError):
             self.assertEqual(E.y, jsons.load('y', E, use_enum_name=False))
+
+    def test_dump_intenum(self):
+        class IE(IntEnum):
+            x = 1
+            y = 2
+
+        self.assertEqual('x', jsons.dump(IE.x))
+        self.assertEqual(2, jsons.dump(IE.y, use_enum_name=False))
+
+    def test_load_intenum(self):
+        class IE(IntEnum):
+            x = 1
+            y = 2
+
+        self.assertEqual(IE.x, jsons.load('x', IE))
+        self.assertEqual(IE.y, jsons.load(2, IE))
+        self.assertEqual(IE.y, jsons.load('y', IE, use_enum_name=True))
+        self.assertEqual(IE.y, jsons.load(2, IE, use_enum_name=False))
+        with self.assertRaises(DeserializationError):
+            self.assertEqual(IE.y, jsons.load(2, IE, use_enum_name=True))
+        with self.assertRaises(DeserializationError):
+            self.assertEqual(IE.y, jsons.load('y', IE, use_enum_name=False))
+
