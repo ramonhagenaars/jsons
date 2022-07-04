60493157a946ec0177d54f7baf3b3b31b9ee1ebb
David Buckley <david@davidbuckley.ca>
2022-07-04 01:53:03 -0400

    add docstrings

--- jsons/deserializers/default_literal.py
+++ jsons/deserializers/default_literal.py
@@ -2,7 +2,18 @@ from typing import Literal, Any, get_args
 
 from jsons.exceptions import DeserializationError
 
-def default_literal_deserializer(obj: Any, cls: Literal, *, strictly_equal_literal: bool = False, **kwargs):
+def default_literal_deserializer(obj: Any, cls: Literal, *, strictly_equal_literal: bool = False, **_):
+    """
+    Deserialize an object to any matching value of the given Literal. The first
+    successful deserialization is returned.
+    :param obj: The object that needs deserializing.
+    :param cls: The Literal type with values (e.g. Literal[1, 2]).
+    :param strictly_equal_literal: determines whether the type of the value
+    and literal should also be taken into account.
+    :param _: not used.
+    :return: An object of the first value of the Literal that could be
+    deserialized successfully.
+    """
     for literal_value in get_args(cls):
         value_equal = obj == literal_value
         type_equal = type(obj) == type(literal_value)
